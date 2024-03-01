644000dee8b3e510063b943d02d8a8d8f91877dc
alwihabsyi <alwihbsyi.ah@gmail.com>
2024-03-01 13:34:17 +0700

    commit

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -32,6 +32,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    lint {
+        checkReleaseBuilds = false
+    }
 }
 
 dependencies {
@@ -41,10 +44,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.annotation:annotation:1.7.1")
 
     // Activity
     implementation("androidx.activity:activity-ktx:1.8.2")
